7ca953e3d45c65a6532e01e2fc9c755948fc6c7c
cayque16 <cayque_santos16@hotmail.com>
2018-07-02 20:43:02 -0300

    q3

---
+++ Prova4/src/questao3/Basico.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package questao3;
+
+/**
+ *
+ * @author cayqu
+ */
+public abstract class Basico {
+    private float cachaca = 100;
+    private int gelo = 2;
+    private float leiteCoco = 50;
+
+    public float getCachaca() {
+        return cachaca;
+    }
+
+    public void setCachaca(float cachaca) {
+        this.cachaca = cachaca;
+    }
+
+    public int getGelo() {
+        return gelo;
+    }
+
+    public void setGelo(int gelo) {
+        this.gelo = gelo;
+    }
+
+    public float getLeiteCoco() {
+        return leiteCoco;
+    }
+
+    public void setLeiteCoco(float leiteCoco) {
+        this.leiteCoco = leiteCoco;
+    }
+    
+    
+}
